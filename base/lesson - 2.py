53e9ea422fa7ed1fede38b521ab229e116d23ebd
juliavas <hhhurz09@icloud.com>
2026-08-14 17:36:42 +0200

    Типы данных

---
+++ base/lesson - 2.py
@@ -0,0 +1,40 @@
+# Типы данных
+
+# Целое число / Integer / int
+my_int = 48
+print(type(my_int))
+
+# Дробное, вещественное, с плавающей точкой число / Float / float
+my_float = 7.8
+print(type(my_float))
+
+# Строка / string / str
+my_str_1 = " Hello"
+my_str_2 = 'World'
+print(type(my_str_2))
+
+# Список / List / list
+my_list = ['Ivan', 7, 150.5]
+print(type(my_list))
+
+# Кортеж / Tuple / tuple
+my_tuple = ("Ivan", 7, 150.5)
+print(type(my_tuple))
+
+# множкество / set / set
+my_set = {1, 1, 1, 2, 2}
+print(my_set)
+
+# словарь / dictionary / dict
+my_dict = {'name': 'Ivan', 'age': 20}
+print(my_dict['name'])
+
+#логический тип / Boolean / bool
+my_bool_1 = True
+my_bool_2 = False
+
+# Примеры сложения переменных
+# print(my_int + my_float)
+# print(my_str_1 + my_str_2)
+# print(my_str_1 + my_int)
+# print(my_int + my_str_1)
