2a41be912e61974714e7c81aef95d55b529aa91b
Mi <hhhurz09@icloud.com>
2026-08-20 23:21:08 +0200

    Домашка :)

---
+++ домашняя работа/hw-1.py
@@ -0,0 +1,40 @@
+# Задание 1
+num = int(input())
+if num % 2 == 0 or num % 1 == .5:
+    print(num)
+else:
+    print(num + 1)
+
+# Задание 2
+num = int(input())
+if num > 0:
+    print(num + 1)
+else:
+    print(num - 1)
+
+# Задание 3
+num = int(input())
+if 0 < num < 100:
+    if num % 3 == 0:
+        print(num // 3)
+    else:
+        print(num * 2)
+
+# Задание 4
+x = int(input())
+if x > 0:
+    print(x * 2 + 5)
+elif x < 0:
+    print( x ** 2 - 1)
+else:
+    print(x)
+
+# Задание 5
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if (x2-x1) == 2 and (y2-y1) == 1 or (x2-x1) == 1 and (y2-y1) == 1:
+    print("Yes")
+else:
+    print("No")
